73dc7cf31c1c1dcc1bf248fe525d75b7c2fd0daf
Jom Dollesin <codewithjom@gmail.com>
2023-07-28 11:05:36 +0800

    fix: minor changes.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <div>
-      <div className='mt-24 mx-auto space-y-4 text-[#7F849C]'>
+      <div className='mt-24 mb-10 mx-auto space-y-4 text-[#7F849C]'>
         <div className='flex justify-center'>
           <Link href='https://github.com/codewithjom'>
             <Image src={'/github.svg'} alt='GitHub logo' width={30} height={30} />
@@ -12,9 +12,9 @@ export default function Footer() {
         </div>
         <div className='flex justify-center'>
           <Link href='#'>
-            <p className='underline italic mr-1'>Uncopyrighted</p>
+            <p className='underline italic mr-1 text-sm'>Uncopyrighted</p>
           </Link>
-          <p className='italic'>and built by Jom Dollesin</p>
+          <p className='italic text-sm'>and built by Jom Dollesin</p>
         </div>
       </div>
     </div>
