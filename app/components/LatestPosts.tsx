59c07a403761054d3357c1b29674e6fe2b6bd218
Jom Dollesin <codewithjom@gmail.com>
2023-07-30 10:52:09 +0800

    fix: removing the margin and show only 3 ids.

--- app/components/LatestPosts.tsx
+++ app/components/LatestPosts.tsx
@@ -8,8 +8,8 @@ export default function LatestPost() {
         <h1 className='text-center text-2xl font-bold my-10 lg:mt-32 lg:text-start'>Latest posts</h1>
         <div>
           <ul className='space-y-4'>
-            {posts.map(item => (
-              <div key={item.id} className='mx-4'>
+            {posts.slice(0, 3).map(item => (
+              <div key={item.id}>
                 <li className='italic text-[#7F849C] text-sm'>{item.date}</li>
                 <li className='text-[#FAB387] underline'>
                   <Link href={item.link}>{item.title}</Link>
