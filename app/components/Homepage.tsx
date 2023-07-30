2538d31ac66a8492d1637a2b991f8b517e4b12b9
Jom Dollesin <codewithjom@gmail.com>
2023-07-30 15:34:17 +0800

    feat: added framer motion.

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { motion } from 'framer-motion'
 import Introduction from './Introduction'
 import LookingForSomethign from './LookingForSomething'
 import LatestPost from './LatestPosts'
@@ -5,13 +8,15 @@ import Footer from './Footer'
 
 export default function Homepage() {
   return (
-    <div className='text-[#CDD6F4]'>
-      <div className='mx-5'>
-        <Introduction />
-        <LookingForSomethign />
-        <LatestPost />
-        <Footer />
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }}>
+      <div className='text-[#CDD6F4]'>
+        <div className='mx-5'>
+          <Introduction />
+          <LookingForSomethign />
+          <LatestPost />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { Montserrat } from 'next/font/google'
 
@@ -10,88 +11,90 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false)
   return (
-    <div>
-      <nav className='w-full bg-[#181825] lg:p-10'>
-        <div className='justify-between px-4 mx-auto lg:max-w-full md:items-center md:flex md:px-8'>
-          <div>
-            <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-              {/* LOGO */}
-              <Link href='/'>
-                <h2 className={`text-2xl text-cyan-600 font-bold ${montserrat.className}`}>Jom Dollesin</h2>
-              </Link>
-              {/* HAMBURGER BUTTON FOR MOBILE */}
-              <div className='md:hidden'>
-                <button
-                  className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <Image src='/close.svg' width={30} height={30} alt='logo' />
-                  ) : (
-                    <Image
-                      src='/hamburger.svg'
-                      width={30}
-                      height={30}
-                      alt='logo'
-                      className='focus:border-none active:border-none'
-                    />
-                  )}
-                </button>
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }}>
+      <div>
+        <nav className='w-full bg-[#181825] lg:p-10'>
+          <div className='justify-between px-4 mx-auto lg:max-w-full md:items-center md:flex md:px-8'>
+            <div>
+              <div className='flex items-center justify-between py-3 md:py-5 md:block'>
+                {/* LOGO */}
+                <Link href='/'>
+                  <h2 className={`text-2xl text-cyan-600 font-bold ${montserrat.className}`}>Jom Dollesin</h2>
+                </Link>
+                {/* HAMBURGER BUTTON FOR MOBILE */}
+                <div className='md:hidden'>
+                  <button
+                    className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {navbar ? (
+                      <Image src='/close.svg' width={30} height={30} alt='logo' />
+                    ) : (
+                      <Image
+                        src='/hamburger.svg'
+                        width={30}
+                        height={30}
+                        alt='logo'
+                        className='focus:border-none active:border-none'
+                      />
+                    )}
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'p-12 md:p-0 block' : 'hidden'
-              }`}
-            >
-              <ul className='h-screen md:h-auto text-[#CDD6F4] items-center justify-center md:flex text-2xl lg:text-base'>
-                <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
-                  <Link href='/projects' onClick={() => setNavbar(!navbar)}>
-                    Projects
-                  </Link>
+            <div>
+              <div
+                className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                  navbar ? 'p-12 md:p-0 block' : 'hidden'
+                }`}
+              >
+                <ul className='h-screen md:h-auto text-[#CDD6F4] items-center justify-center md:flex text-2xl lg:text-base'>
+                  <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
+                    <Link href='/projects' onClick={() => setNavbar(!navbar)}>
+                      Projects
+                    </Link>
+                  </li>
+                  <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
+                    <Link href='#blogs' onClick={() => setNavbar(!navbar)}>
+                      Blog
+                    </Link>
+                  </li>
+                  <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
+                    <Link href='/uses' onClick={() => setNavbar(!navbar)}>
+                      Uses
+                    </Link>
+                  </li>
+                  <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
+                    <Link href='#links' onClick={() => setNavbar(!navbar)}>
+                      Links
+                    </Link>
+                  </li>
+                  <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
+                    <Link href='/contact' onClick={() => setNavbar(!navbar)}>
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className='hidden md:block'>
+              <ul className='flex space-x-8'>
+                <li>
+                  <Image src='/github.svg' alt='github logo' width={30} height={30} />
                 </li>
-                <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
-                  <Link href='#blogs' onClick={() => setNavbar(!navbar)}>
-                    Blog
-                  </Link>
-                </li>
-                <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
-                  <Link href='/uses' onClick={() => setNavbar(!navbar)}>
-                    Uses
-                  </Link>
-                </li>
-                <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
-                  <Link href='#links' onClick={() => setNavbar(!navbar)}>
-                    Links
-                  </Link>
-                </li>
-                <li className='py-2 px-6 text-center underline md:hover:text-[#FAB387] md:hover:bg-transparent'>
-                  <Link href='/contact' onClick={() => setNavbar(!navbar)}>
-                    Contact
+                <li>
+                  <Link
+                    href='#'
+                    className='text-[#CDD6F4] px-8 py-4 border border-[#CDD6F4] rounded-lg hover:bg-[#cdd6f433]'
+                  >
+                    Resume
                   </Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className='hidden md:block'>
-            <ul className='flex space-x-8'>
-              <li>
-                <Image src='/github.svg' alt='github logo' width={30} height={30} />
-              </li>
-              <li>
-                <Link
-                  href='#'
-                  className='text-[#CDD6F4] px-8 py-4 border border-[#CDD6F4] rounded-lg hover:bg-[#cdd6f433]'
-                >
-                  Resume
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </motion.div>
   )
 }
